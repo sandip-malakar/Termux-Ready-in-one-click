1b6c10fbd0a480c8642b0b780443bd12502d8c9a
sandip-malakar <62707172+sandip-malakar@users.noreply.github.com>
2020-03-31 14:26:47 +0530

    Add files via upload
    This is tremendous tool to ready your termux completely..

---
+++ Termux.py
@@ -0,0 +1,25 @@
+#! /bin/usr/python2
+import os 
+import sys 
+
+
+
+#script start 
+print("\033[1;32m")
+print("Coded by Mandeep")
+print("\033[1;31m")
+os.system("pkg install figlet -y")
+os.system("figlet start Installing")
+os.system("pkg install python -y")
+os.system("pkg install python2 -y")
+os.system("pkg install git -y")
+os.system("pkg install toilet -y")
+print("\033[1;33m")
+os.system("toilet -f mono12 -F border Work in progress")
+print("\033[1;32m")
+os.system("pkg install ruby -y")
+os.system("pkg install gem -y")
+os.system("pkg install sl -y")
+os.system("pkg install w3m -y")
+os.system("figlet Done !!")
+
